5945a46ea94401b6fef2b471050f6800a5333964
chinweokwu <paulcmorah@gmail.com>
2022-08-31 01:53:25 +0100

    add navbar

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 import ExerciseDetail from './pages/ExerciseDetail';
 import Home from './pages/Home'
-import Bavbar fo
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import './App.css';
 
 const App = () => {
   return (
-    <Box>
+    <Box width="400px" sx={{width: {xl: '1488px'}}} m='auto'>
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/exercise/:id" element={<ExerciseDetail/>}/>
       </Routes>
+      <Footer/>
     </Box>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import App from './App';
 
 const root = ReactDom.createRoot(document.getElementById('root'))
 
-root.render(<App/>)
+root.render(
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+)
