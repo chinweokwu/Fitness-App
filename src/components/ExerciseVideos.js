2907cb37d41a2dbbc47451e34fd4aee7992900b8
chinweokwu <paulcmorah@gmail.com>
2022-09-01 23:57:42 +0100

    add exercise youtube videos

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,8 +1,47 @@
-import React from 'react'
+import React from 'react';
+import {Box, Typography, Stack} from '@mui/material';
 
-const ExerciseVideos = () => {
+
+const ExerciseVideos = ({exerciseVideo, name}) => {
+  if(!exerciseVideo.length) return 'loading...';
+
   return (
-    <div>ExerciseVideos</div>
+    <Box sx={{marginTop: {lg: '200px', xs: '20px'}}} p='20px'>
+      <Typography variant="h3" mb="33px">
+        watch <span style={{color: '#ff2625', textTransform: 'capitalize'}}>{name}</span> workout videos
+      </Typography>
+      <Stack 
+        justifyContent='flex-start' 
+        flexWrap="wrap" 
+        alignItem="center"
+        sx={{
+          flexDirection: {lg: 'row'},
+          gap: {lg: '110px', xs: '0'}
+        }}
+        >
+          {
+            exerciseVideo?.slice(0,6).map((item, index) => (
+              <a
+                key={index}
+                className="exercise-video"
+                href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
+                target="_blank"
+                rel="nonreferrer noreferrer"
+              >
+                <img src={item.video.thumbnails[0].url} alt={item.video.title}/>
+                <Box>
+                  <Typography variant='h5' color='#000'>
+                    {item.video.title}
+                  </Typography>
+                  <Typography variant='h6' color='#000'>
+                    {item.video.channelName}
+                  </Typography>
+                </Box>
+              </a>
+            ))
+          }
+      </Stack>
+    </Box>
   )
 }
 
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,6 +1,7 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React,{useEffect, useState} from 'react'
 import {useParams} from 'react-router-dom';
-import { exerciseOptions, fetchData } from '../utils/fetchData';
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
 import ExerciseVideos from '../components/ExerciseVideos';
 import Detail from '../components/Detail';
 import SimilarExercises from '../components/SimilarExercises';
@@ -8,14 +9,19 @@ import {Box} from '@mui/material'
 
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideo, setExerciseVideo] = useState([]);
   const {id} = useParams();
 
   useEffect(() => {
     const fetchExercisesData = async () => {
       const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com'
       const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com'
+      
       const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`,exerciseOptions);
       setExerciseDetail(exerciseDetailData)
+
+      const exerciseVideoData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`,youtubeOptions);
+      setExerciseVideo(exerciseVideoData.contents)
     }
     fetchExercisesData()
   }, [id])
@@ -23,7 +29,7 @@ const ExerciseDetail = () => {
   return (
     <Box>
       <Detail exerciseDetail={exerciseDetail}/>
-      <ExerciseVideos/>
+      <ExerciseVideos exerciseVideo={exerciseVideo} name={exerciseDetail.name}/>
       <SimilarExercises/>
     </Box>
   )
